7ab95ca74033ee9e2b5dbab4d17402d8ca65649f
Hemang Bhogayata <hemang249@gmail.com>
2019-11-23 20:27:27 +0530

    Made the Header Sticky

--- js/index.js
+++ js/index.js
@@ -2,3 +2,21 @@ document.getElementById('btn-knowmore').addEventListener('click',(e)=>{
 	document.getElementById('about').scrollIntoView()
 })
 
+window.onscroll = () => makeSticky()
+
+// Get the header
+var header = document.getElementById("header");
+
+// Get the offset position of the navbar
+
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function makeSticky() {
+  const headerOffSet = header.offsetTop
+
+  if (window.pageYOffset > headerOffSet) {
+    header.classList.add("sticky")
+  } else {
+    header.classList.remove("sticky")
+  }
+} 
